f9f42cdaae5e9b750f608b021b2c7d0b13edf4a0
Bhushan91086 <bhushanarvekar@gmail.com>
2023-10-16 19:08:59 +0530

    Order API implementation

---
+++ EasyGroceries.Order.Application/DTOs/OrderHeaderDto.cs
@@ -0,0 +1,19 @@
+﻿using EasyGroceries.Order.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGroceries.Order.Application.DTOs
+{
+    public class OrderHeaderDto
+    {
+        public int OrderHeaderId { get; set; }
+        public int UserId { get; set; }
+        public bool LoyaltyMembershipOpted { get; set; }
+        public double OrderTotal { get; set; }
+        public OrderStatusEnum OrderStatus { get; set; }
+        public IEnumerable<OrderDetailsDto> OrderDetails { get; set; }
+    }
+}
---
+++ EasyGroceries.Services.OrderAPI/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using EasyGroceries.Order.Application.Contracts.Services;
+using EasyGroceries.Order.Application.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyGroceries.Services.OrderAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpGet("GetOrders")]
+        public async Task<ActionResult<ResponseDto<OrderHeaderDto>>> Get(int userId)
+        {
+            var response = await _orderService.GetOrderByUserId(userId);
+            if (response.Result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+
+        [HttpPost("CreateOrder")]
+        public async Task<ActionResult<ResponseDto<OrderHeaderDto>>> CreateOrder([FromBody] CartDto cartDto)
+        {
+            var response = await _orderService.CreateOrder(cartDto);
+            return Ok(response);
+        }
+    }
+}
